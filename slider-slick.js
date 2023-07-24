d8c21b3f48fe82f3b2e8a25a03c6dde63fd78c6e
beatrizcdsmartins <beatrizcdsmartins@gmail.com>
2023-07-24 10:12:23 -0300

    correction

---
+++ slider-slick.js
@@ -0,0 +1,13 @@
+$('.carousel-comercial').slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  });
+
+  $('.carousel-empresarial').slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  });
